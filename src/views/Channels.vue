5e77af24e348db6c4e0abf41ad2b23a6cd342533
The Real Tinky Winky <tinkywinky@teletubbies.com>
2021-02-28 02:14:22 -0300

    Cowabunga!

---
+++ src/views/Channels.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-overlay :show="overlay" variant="secondary" no-wrap></b-overlay>
+
+    <b-container fluid class="h-100">
+      <b-row align-v="center" class="h-100">
+        <b-col>
+          <b-table striped hover :items="items"></b-table>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      overlay: true,
+      items: []
+    }
+  }
+}
+</script>
